407013d12de5ddc9d43e4de0d6488bfecd6bb022
tp023988 <tp023988@gmail.com>
2020-01-29 16:53:03 +0600

    Add files via upload

---
+++ index_js.js
@@ -0,0 +1,7 @@
+function test_1(a)
+{
+  var i1 = document.querySelector("#i_1").value
+  var i2 = document.querySelector("#i_1").value
+  console.log("var 1:"+i1+"; parameter:"+a)
+  console.log("var 2:"+i2)
+}
